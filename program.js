322ae10763c5d20893c7763853211c6cb5955966
Julio Makdisse Saito <saitodisse@gmail.com>
2014-10-27 10:35:42 -0200

    INPUT OUTPUT

--- program.js
+++ program.js
@@ -1,9 +1,10 @@
 'use strict';
 
-var fs = require('fs');
+// var fs = require('fs');
+// var filePath = process.argv[2];
+// var from = fs.createReadStream(filePath);
 
-var filePath = process.argv[2];
-var from = fs.createReadStream(filePath);
+var from = process.stdin;
 
 var destination = process.stdout;
 
