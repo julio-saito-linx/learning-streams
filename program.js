df8d8b4ad099f86d6ef0d1dc5cf983c11f67f836
Julio Makdisse Saito <saitodisse@gmail.com>
2014-10-27 10:28:38 -0200

    MEET PIPE

--- program.js
+++ program.js
@@ -1,1 +1,10 @@
-console.log('beep boop');
+'use strict';
+
+var fs = require('fs');
+
+var filePath = process.argv[2];
+var from = fs.createReadStream(filePath);
+
+var destination = process.stdout;
+
+from.pipe(destination);
